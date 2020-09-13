51c9f6d813863a24d28c9b67c0d9bc8dd1786789
Katiuska <katiuskaalicea@gmail.com>
2020-09-12 17:53:40 -0700

    First Commit

---
+++ user-card/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import UserCard from "./Components/UserCard";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>This is the App Component</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ user-card/src/Components/UserCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class UserCard extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>This is the UserCard component</h2>
+      </div>
+    );
+  }
+}
+
+export default UserCard;

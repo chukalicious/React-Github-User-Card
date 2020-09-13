901d08872f84e6e5dcb5bc3d7d87be3ca008519e
Katiuska <katiuskaalicea@gmail.com>
2020-09-12 19:16:43 -0700

    Had to create a multiple axios call the get the info on the followers. That meant I had to bereak my previous one and at this point the bind in the jsx is broken but the data of both calls has been added to App.js state using the componentDidMount() method

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -8,17 +8,35 @@ class App extends React.Component {
     super();
     this.state = {
       userInfo: {},
+      followersInfo: [{}],
     };
   }
 
   componentDidMount() {
+    let one = "https://api.github.com/users/chukalicious";
+    let two = "https://api.github.com/users/chukalicious/followers";
+
+    const requestOne = axios.get(one);
+    const requestTwo = axios.get(two);
+
     axios
-      .get("https://api.github.com/users/chukalicious")
-      .then((res) => this.setState({ userInfo: res.data }))
-      .catch((err) => console.log(err));
+      .all([requestOne, requestTwo])
+      .then(
+        axios.spread((...responses) => {
+          const responseOne = responses[0];
+          const responseTwo = responses[1];
+          console.log("Response one:", responseOne);
+          console.log("Response Two:", responseTwo.data);
+          // use/access the results
+        })
+      )
+      .catch((errors) => {
+        // react on errors.
+      });
   }
 
   render() {
+    console.log("first render state: ", this.state);
     return (
       <div>
         <h1>Github User Card</h1>

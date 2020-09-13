0dd91e9af9ba765b33c26d743eadac59262ca1f7
Katiuska <katiuskaalicea@gmail.com>
2020-09-12 18:32:05 -0700

    created usercard component, chasnged App from functional component to class component, set up axios call to get our data from inside the componentDidMount method

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,13 +1,33 @@
 import React from "react";
 import UserCard from "./Components/UserCard";
 import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div>
-      <h1>This is the App Component</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userInfo: {},
+    };
+  }
+
+  componentDidMount() {
+    console.log("CDM!");
+    axios
+      .get("https://api.github.com/users/chukalicious")
+      .then((res) => this.setState({ userInfo: res.data }))
+      .catch((err) => console.log(err));
+  }
+
+  render() {
+    console.log("data from inside the render() in App", this.state.data);
+    return (
+      <div>
+        <h1>This is the App Component</h1>
+        <UserCard user={this.state} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- user-card/src/Components/UserCard.js
+++ user-card/src/Components/UserCard.js
@@ -2,9 +2,11 @@ import React from "react";
 
 class UserCard extends React.Component {
   render() {
+    console.log("props passed to userCard component", this.props);
     return (
       <div>
         <h2>This is the UserCard component</h2>
+        <h3>Username: </h3>
       </div>
     );
   }
